6282ccbd737276cdb3d34cbe5b1dd6c0478ca93e
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2023-03-24 10:50:48 +0100

    First steps for JS Select option

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-/*function updateCountry() {
-
+function updateCountry(event) {
+  let country = event.target.value;
+  if (country === "hungary") {
+  }
 }
 
 let selectCountryElement = document.querySelector("#country");
 selectCountryElement.addEventListener("change", updateCountry);
 
 let hungaryElement = document.querySelector("#hungary");
-let albaniaElement = document.querySelector("#albania"); */
+let albaniaElement = document.querySelector("#albania");
