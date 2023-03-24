cc2300eb6cb8e20e698c21b696b40adf72a7b2c3
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2023-03-24 10:47:45 +0100

    Color changes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-let selectCountry = document.querySelector("select");
+/*function updateCountry() {
+
+}
+
+let selectCountryElement = document.querySelector("#country");
+selectCountryElement.addEventListener("change", updateCountry);
+
+let hungaryElement = document.querySelector("#hungary");
+let albaniaElement = document.querySelector("#albania"); */
